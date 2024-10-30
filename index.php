4f18d5d840b39f0cfe492584d7207ec652036adf
BedrettinGuelen <bedrettingulen@gmail.com>
2024-10-30 22:31:41 +0100

    add singleton Class

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once "SingletonPattern/Preference.php";
+
+echo "Begin Singleton.\n";
+$pref1 = Preference::createInstance();
+$pref2 = Preference::createInstance();
+
+if($pref1 === $pref2)
+{
+    echo "Singleton is working.\n";
+    $pref1->greetingSingleton();
+    $pref2->greetingSingleton();
+}
+else
+{
+    echo "Singleton is not working.\n";
+}
+
